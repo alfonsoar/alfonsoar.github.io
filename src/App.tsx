b135bfa61dd37cbf412a69b45f57322ccc2d9fc2
agar23 <aga23@njit.edu>
2019-06-05 23:25:15 -0400

    add sass & code coverage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, } from 'react-router-dom';
-import HomePage from './home';
+import HomePage from './pages/home';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
