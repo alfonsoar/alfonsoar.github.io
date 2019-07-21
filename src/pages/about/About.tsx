881a105bc67027d5d7467bf76fd931e11fa88619
Alfonso <aaustinrivera@gmail.com>
2019-07-21 16:12:57 -0400

    update about

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -5,33 +5,31 @@ import ProfileImage from "../../resources/Profile.png";
 
 const AboutPage: React.FC = () => {
   return (
-      <Container>
-        <Row className="container-title">
-          <Col className="container-title title">
-            <span>About Me</span>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-          <img src={ProfileImage} alt="Logo" className="profileImage"/>
-          </Col>
-          <Col>
-            <div>
-              <p className="body">
-                Hello! My name is Alfonso Austin and I am technologist based out
-                of NYC. I am currently working as a full stack developer in the
-                finance industry. I have experience working with programming
-                languages such as: JavaScript, HTML, Python, PHP and Java.
+    <Container>
+      <Row>
+        <Col className="container-profile">
+          <img src={ProfileImage} alt="Logo" className="profileImage" />
+        </Col>
+        <Col>
+          <div className="container-title">
+            <span className="container-title title">About Me</span>
+          </div>
+          <div>
+            <p className="body">
+              Hello! My name is Alfonso Austin and I am technologist based out
+              of NYC. I am currently working as a full stack developer in the
+              finance industry. I have experience working with programming
+              languages such as: JavaScript, HTML, Python, PHP and Java.
               </p>
-              <p className="body">
-                My interest include building awesome things, learning/tinkering
-                with new technologies and all things space related. If you wish
-                to get in contact with me please refer to the contact page.
+            <p className="body">
+              My interest include building awesome things, learning/tinkering
+              with new technologies and all things space related. If you wish
+              to get in contact with me please refer to the contact page.
               </p>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
