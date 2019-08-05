9a4d71db945e6a4ec977564054a45b7d089c77f5
Alfonso <aaustinrivera@gmail.com>
2019-08-04 22:11:40 -0400

    refactor about page

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,36 +1,34 @@
 import * as React from "react";
 import { Container, Row, Col } from "reactstrap";
 
+import TypingAnimation from '../../components/TypingAnimation';
 import ProfileImage from "../../resources/Profile.png";
 
-const AboutPage: React.FC = () => {
-  return (
-    <Container>
-      <Row>
-        <Col className="container-profile">
-          <img src={ProfileImage} alt="Logo" className="profileImage" />
-        </Col>
-        <Col>
-          <div className="container-title">
-            <span className="container-title title">About Me</span>
-          </div>
-          <div>
+class AboutPage extends React.Component<{}, {}>  {
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col className="container-profile">
+            <img src={ProfileImage} alt="Logo" className="profileImage" />
+          </Col>
+          <Col>
+            <div className="container-title">
+              <span className="container-title title">Alfonso Austin</span>
+              <br />
+              <TypingAnimation strings={['I am a Software Developer']}/>
+            </div>
+            <br />
+            <br />
             <p className="body">
-              Hello! My name is Alfonso Austin and I am technologist based out
-              of NYC. I am currently working as a full stack developer in the
-              finance industry. I have experience working with programming
-              languages such as: JavaScript, HTML, Python, PHP and Java.
-              </p>
-            <p className="body">
-              My interest include building awesome things, learning/tinkering
-              with new technologies and all things space related. If you wish
-              to get in contact with me please refer to the contact page.
-              </p>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  );
+              Currently working in the finance industry. My interest include building awesome things, 
+              learning/tinkering with new technologies and all things space related
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 };
 
 export default AboutPage;
