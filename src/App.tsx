8d7806e0f38012b8e2d434b3a17a56415dfabf2a
Alfonso Austin-Rivera <alfoaust@amazon.com>
2023-08-09 23:31:38 -0400

    fix: use p-3 class

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Row, Col } from "reactstrap";
+import { Row } from "reactstrap";
 
-import { AboutPage } from './pages/about';
+import { AboutPage } from "./pages/about";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 
@@ -9,20 +9,14 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <Row className="no-gutters">
-          <Col>
-          <Header />
-          </Col>
-        </Row>
-        <Row className="no-gutters align-items-center">
-          <Col>
-          <AboutPage />
-          </Col>
-        </Row>
-        <Row className="no-gutters">
-          <Col>
-            <Footer />
-          </Col>
-        </Row>
+        <Header />
+      </Row>
+      <Row className="no-gutters align-items-center p-3">
+        <AboutPage />
+      </Row>
+      <Row className="no-gutters">
+        <Footer />
+      </Row>
     </div>
   );
 };
